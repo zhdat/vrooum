4d6a78e16c1bf8882a590939f5ec1e170795728f
ravix <calliste.ravix@ecole.ensicaen.fr>
2023-05-01 15:56:15 +0200

    follow_line

--- GrandPrix2023_4.0.3/Picole_Nationale/droitAuBut.c
+++ GrandPrix2023_4.0.3/Picole_Nationale/droitAuBut.c
@@ -8,7 +8,6 @@
 #define BOOSTS_AT_START 5
 
 #include <limits.h>
-#include "follow_line.h"
 #define INF INT_MAX
 
 typedef struct {
@@ -20,6 +19,107 @@ typedef struct {
 	int cost;
 	Position pos;
 } Node;
+
+/* follow_line */
+
+/**
+ * Position on a map (integer coordinates)
+ */
+typedef struct {
+	int x;
+	int y;
+} Pos2Dint;
+
+/**
+ * Position with floating point coordinates
+ */
+typedef struct {
+	float x;
+	float y;
+} Pos2Dfloat;
+
+/**
+ * Dicrete line traversal information
+ */
+typedef struct {
+	Pos2Dint start;
+	Pos2Dint end;
+	Pos2Dfloat currentPosition;
+	Pos2Dfloat delta; /*< Shift vector */
+	int len;		  /*< Length along the main axis */
+	int pos;		  /*< Reference position on the line */
+} InfoLine;
+
+/**
+ * Initialize an InfoLine data structure
+ */
+void initLine(int x1, int y1, int x2, int y2, InfoLine* infoLine)
+{
+	int adxi, adyi, dxi, dyi;
+	infoLine->start.x = x1;
+	infoLine->start.y = y1;
+	infoLine->currentPosition.x = x1 + 0.5;
+	infoLine->currentPosition.y = y1 + 0.5;
+	infoLine->end.x = x2;
+	infoLine->end.y = y2;
+
+	adxi = dxi = x2 - x1;
+	adyi = dyi = y2 - y1;
+	if (adxi < 0) {
+		adxi = -dxi;
+	}
+	if (adyi < 0) {
+		adyi = -dyi;
+	}
+	infoLine->pos = 0;
+	infoLine->len = adxi;
+	if (adyi > adxi) {
+		infoLine->len = adyi;
+	}
+	infoLine->delta.x = ((float)dxi) / infoLine->len;
+	infoLine->delta.y = ((float)dyi) / infoLine->len;
+}
+
+/**
+ * @brief nextPoint Move a point along a line in one direction
+ *
+ * @param infoLine Line traversal information
+ * @param point current/next position
+ * @param direction (+1 forward, -1 backward)
+ * @return 1 if point is a new point in the line, otherwise -1 (info is
+ *         already at the end point)
+ */
+int nextPoint(InfoLine* infoLine, Pos2Dint* point, int direction)
+{
+	if (direction > 0) {
+		if (infoLine->pos == infoLine->len) {
+			point->x = infoLine->end.x;
+			point->y = infoLine->end.y;
+			return -1; /* End of the line */
+		}
+		infoLine->currentPosition.x += infoLine->delta.x;
+		infoLine->currentPosition.y += infoLine->delta.y;
+		point->x = ((int)infoLine->currentPosition.x);
+		point->y = ((int)infoLine->currentPosition.y);
+		infoLine->pos++;
+		return 1; /* a new point is found */
+	}
+	if (direction < 0) {
+		if (infoLine->pos == 0) {
+			point->x = infoLine->start.x;
+			point->y = infoLine->start.y;
+			return -1; /* End of the line */
+		}
+		infoLine->currentPosition.x -= infoLine->delta.x;
+		infoLine->currentPosition.y -= infoLine->delta.y;
+		point->x = ((int)infoLine->currentPosition.x);
+		point->y = ((int)infoLine->currentPosition.y);
+		infoLine->pos--;
+
+		return 1; /* a new point is found */
+	}
+	return 1;	  /* direction == 0 => no motion */
+}
 
 int is_valid_move(char** map, int x, int y, int width, int height)
 {
