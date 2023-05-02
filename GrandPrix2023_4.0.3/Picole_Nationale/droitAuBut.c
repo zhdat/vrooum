77611bf5633240ca6bca698bff31162d9caee6ab
ravix <calliste.ravix@ecole.ensicaen.fr>
2023-05-02 08:29:08 +0200

    retour version 1

--- GrandPrix2023_4.0.3/Picole_Nationale/droitAuBut.c
+++ GrandPrix2023_4.0.3/Picole_Nationale/droitAuBut.c
@@ -1,21 +1,46 @@
+#include <limits.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <limits.h>
+
 #define MAX_LINE_LENGTH 1024
 #define BOOSTS_AT_START 5
 
+#include <limits.h>
+#define INF INT_MAX
+
+typedef struct {
+	int x;
+	int y;
+} Position;
+
+typedef struct {
+	int cost;
+	Position pos;
+} Node;
+
+/* follow_line */
+
+/**
+ * Position on a map (integer coordinates)
+ */
 typedef struct {
 	int x;
 	int y;
 } Pos2Dint;
 
+/**
+ * Position with floating point coordinates
+ */
 typedef struct {
 	float x;
 	float y;
 } Pos2Dfloat;
 
+/**
+ * Dicrete line traversal information
+ */
 typedef struct {
 	Pos2Dint start;
 	Pos2Dint end;
@@ -25,6 +50,9 @@ typedef struct {
 	int pos;		  /*< Reference position on the line */
 } InfoLine;
 
+/**
+ * Initialize an InfoLine data structure
+ */
 void initLine(int x1, int y1, int x2, int y2, InfoLine* infoLine)
 {
 	int adxi, adyi, dxi, dyi;
@@ -52,6 +80,15 @@ void initLine(int x1, int y1, int x2, int y2, InfoLine* infoLine)
 	infoLine->delta.y = ((float)dyi) / infoLine->len;
 }
 
+/**
+ * @brief nextPoint Move a point along a line in one direction
+ *
+ * @param infoLine Line traversal information
+ * @param point current/next position
+ * @param direction (+1 forward, -1 backward)
+ * @return 1 if point is a new point in the line, otherwise -1 (info is
+ *         already at the end point)
+ */
 int nextPoint(InfoLine* infoLine, Pos2Dint* point, int direction)
 {
 	if (direction > 0) {
@@ -84,6 +121,132 @@ int nextPoint(InfoLine* infoLine, Pos2Dint* point, int direction)
 	return 1;	  /* direction == 0 => no motion */
 }
 
+int is_valid_move(char** map, int x, int y, int width, int height)
+{
+	if (x < 0 || x >= width || y < 0 || y >= height) {
+		return 0;
+	}
+	return map[y][x] != '.';
+}
+
+int get_cost(char terrain)
+{
+	if (terrain == '#') {
+		return 1;
+	} else if (terrain == '~') {
+		return 5;
+	} else {
+		return INF;
+	}
+}
+
+Position dijkstra_next_move(char** map, int width, int height, Position start, Position end)
+{
+	int** visited;
+	int** dist;
+	int y;
+	int x;
+	int i;
+	Position** prev;
+	Position directions[] = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 }, { -1, -1 }, { 1, 1 }, { -1, 1 }, { 1, -1 } };
+	int dir_count;
+
+	visited = malloc(height * sizeof(int*));
+	if (visited == NULL) {
+		perror("Memory allocation failed");
+	}
+	for (i = 0; i < height; i++) {
+		visited[i] = malloc(width * sizeof(int));
+		if (visited[i] == NULL) {
+			perror("Memory allocation failed");
+		}
+	}
+
+	dist = malloc(height * sizeof(int*));
+	if (dist == NULL) {
+		perror("Memory allocation failed");
+	}
+	for (i = 0; i < height; i++) {
+		dist[i] = malloc(width * sizeof(int));
+		if (dist[i] == NULL) {
+			perror("Memory allocation failed");
+		}
+	}
+	prev = malloc(height * sizeof(Position*));
+	if (prev == NULL) {
+		perror("Memory allocation failed");
+	}
+	for (i = 0; i < height; i++) {
+		prev[i] = malloc(width * sizeof(Position));
+		if (prev[i] == NULL) {
+			perror("Memory allocation failed");
+		}
+	}
+
+	dir_count = sizeof(directions) / sizeof(directions[0]);
+
+	for (y = 0; y < height; y++) {
+		for (x = 0; x < width; x++) {
+			visited[y][x] = 0;
+			dist[y][x] = INF;
+		}
+	}
+
+	dist[start.y][start.x] = 0;
+
+	while (1) {
+		Node min_node;
+		min_node.cost = INF;
+
+		for (y = 0; y < height; y++) {
+			for (x = 0; x < width; x++) {
+				if (!visited[y][x] && dist[y][x] < min_node.cost) {
+					min_node.cost = dist[y][x];
+					min_node.pos.x = x;
+					min_node.pos.y = y;
+				}
+			}
+		}
+
+		if (min_node.cost == INF || (min_node.pos.x == end.x && min_node.pos.y == end.y)) {
+			break;
+		}
+
+		x = min_node.pos.x;
+		y = min_node.pos.y;
+		visited[y][x] = 1;
+
+		for (i = 0; i < dir_count; i++) {
+			int dx = directions[i].x;
+			int dy = directions[i].y;
+			int nx = x + dx;
+			int ny = y + dy;
+
+			if (is_valid_move(map, nx, ny, width, height) && !visited[ny][nx]) {
+				int new_cost = dist[y][x] + get_cost(map[ny][nx]);
+				if (new_cost < dist[ny][nx]) {
+					dist[ny][nx] = new_cost;
+					prev[ny][nx].x = x;
+					prev[ny][nx].y = y;
+				}
+			}
+		}
+	}
+
+	if (dist[end.y][end.x] == INF) {
+		Position pos;
+		pos.x = -1;
+		pos.y = -1;
+		return pos;
+	} else {
+		Position current = end;
+		while (!(prev[current.y][current.x].x == start.x && prev[current.y][current.x].y == start.y)) {
+			current = prev[current.y][current.x];
+		}
+		return current;
+	}
+}
+
 /**
  * @brief Compute the gas consumption of a requested acceleration
  *
@@ -94,7 +257,7 @@ int nextPoint(InfoLine* infoLine, Pos2Dint* point, int direction)
  * @param accY Acceleration y component
  * @param speedX Speed x component
  * @param speedY Speed y component
- * @param inSand (boolean)
+ * @param inSand (0 or 1)
  * @return Number of gas units consumed
  */
 int gasConsumption(int accX, int accY, int speedX, int speedY, int inSand)
@@ -107,178 +270,6 @@ int gasConsumption(int accX, int accY, int speedX, int speedY, int inSand)
 	return -gas;
 }
 
-Pos2Dint calculate_optimal_acceleration(int myX, int myY, int speedX, int speedY, int gasLevel, int width, int height, char** map)
-{
-	int accX;
-	int accY;
-	Pos2Dint optimalAcceleration = { 0, 0 };
-	int minGasCost = 1000000;
-	for (accX = -1; accX <= 1; accX++) {
-		for (accY = -1; accY <= 1; accY++) {
-			int newX = myX + speedX + accX;
-			int newY = myY + speedY + accY;
-			if (newX >= 0 && newX < width && newY >= 0 && newY < height && map[newY][newX] != '.') {
-				int gasCost = gasConsumption(accX, accY, speedX, speedY, map[newY][newX] == '~');
-				if (gasCost < minGasCost && gasLevel + gasCost > 0) {
-					minGasCost = gasCost;
-					optimalAcceleration.x = accX;
-					optimalAcceleration.y = accY;
-				}
-			}
-		}
-	}
-
-	return optimalAcceleration;
-}
-
-int detect_collisions(int myX, int myY, int speedX, int speedY, int width, int height, char** map)
-{
-	Pos2Dint start;
-	Pos2Dint end;
-	InfoLine lineInfo;
-	start.x = myX;
-	start.y = myY;
-	end.x = myX + speedX;
-	end.y = myY + speedY;
-
-	if (end.x < 0 || end.x >= width || end.y < 0 || end.y >= height) {
-		return 1;
-	}
-
-	initLine(start.x, start.y, end.x, end.y, &lineInfo);
-
-	Pos2Dint currentPosition;
-	while (nextPoint(&lineInfo, &currentPosition, +1) > 0) {
-		char terrain = map[currentPosition.y][currentPosition.x];
-
-		if (terrain == '#') {
-			return 1;
-		}
-	}
-
-	return 0;
-}
-
-void avoid_obstacles(int* accelerationX, int* accelerationY, int speedX, int speedY, int myX, int myY, int width, int height, char** map)
-{
-	int accX;
-	int accY;
-	int bestAccX = 0, bestAccY = 0;
-	int minCollisionDistance = -1;
-
-	for (accX = -1; accX <= 1; ++accX) {
-		for (accY = -1; accY <= 1; ++accY) {
-			int newX = myX + speedX + accX;
-			int newY = myY + speedY + accY;
-
-			if (newX >= 0 && newX < width && newY >= 0 && newY < height) {
-				int collisionDistance = 0;
-
-				while (!detect_collisions(myX, myY, speedX + accX, speedY + accY, width, height, map)) {
-					myX += speedX + accX;
-					myY += speedY + accY;
-					collisionDistance++;
-				}
-
-				if (collisionDistance > minCollisionDistance) {
-					minCollisionDistance = collisionDistance;
-					bestAccX = accX;
-					bestAccY = accY;
-				}
-			}
-		}
-	}
-
-	*accelerationX = bestAccX;
-	*accelerationY = bestAccY;
-}
-
-int find_shortest_path(int startX, int startY, int endX, int endY, int width, int height, char** map)
-{
-	int distance[height][width];
-	int visited[height][width];
-	int i, j;
-
-	for (i = 0; i < height; i++) {
-		for (j = 0; j < width; j++) {
-			distance[i][j] = INT_MAX;
-			visited[i][j] = 0;
-		}
-	}
-
-	distance[startY][startX] = 0;
-
-	while (1) {
-		int min_distance = INT_MAX;
-		int min_x = -1;
-		int min_y = -1;
-
-		for (i = 0; i < height; i++) {
-			for (j = 0; j < width; j++) {
-				if (!visited[i][j] && distance[i][j] < min_distance) {
-					min_distance = distance[i][j];
-					min_x = j;
-					min_y = i;
-				}
-			}
-		}
-
-		if (min_x == -1 && min_y == -1) {
-			break;
-		}
-
-		if (min_x == endX && min_y == endY) {
-			break;
-		}
-
-		visited[min_y][min_x] = 1;
-
-		for (i = -1; i <= 1; i++) {
-			for (j = -1; j <= 1; j++) {
-				int new_x = min_x + j;
-				int new_y = min_y + i;
-
-				if (new_x >= 0 && new_y >= 0 && new_x < width && new_y < height && map[new_y][new_x] != '.' && !visited[new_y][new_x]) {
-					int new_distance = distance[min_y][min_x] + 1;
-					if (new_distance < distance[new_y][new_x]) {
-						distance[new_y][new_x] = new_distance;
-					}
-				}
-			}
-		}
-	}
-
-	return distance[endY][endX];
-}
-
-int apply_boost(int* boosts, int* accelerationX, int* accelerationY, int speedX, int speedY, int myX, int myY, int width, int height, char** map)
-{
-	int i;
-	if (*boosts <= 0) {
-		return 0;
-	}
-
-	int straight_line_length = 0;
-	for (i = 1; i <= 5; ++i) {
-		int newX = myX + i * (*accelerationX);
-		int newY = myY + i * (*accelerationY);
-		if (newX >= 0 && newX < width && newY >= 0 && newY < height && map[newY][newX] != '#') {
-			straight_line_length++;
-		} else {
-			break;
-		}
-	}
-
-	if (straight_line_length >= 4) {
-		*boosts -= 1;
-		*accelerationX *= 2;
-		*accelerationY *= 2;
-		return 1;
-	}
-
-	return 0;
-}
-
 int main()
 {
 	int row;
@@ -287,13 +278,19 @@ int main()
 	int boosts = BOOSTS_AT_START;
 	int round = 0;
 	int accelerationX = 1, accelerationY = 0;
+	int accelerationX_old = 0, accelerationY_old = 0;
 	int speedX = 0, speedY = 0;
 	char action[100];
 	char line_buffer[MAX_LINE_LENGTH];
 	char** grid;
-	Pos2Dint accel;
-	int collision;
-	int pathDistance;
+	int x;
+	int y;
+	int j;
+	Position start;
+	Position end;
+	Position next;
+	InfoLine lineInfo;
+	Pos2Dint currentPoint;
 
 	boosts = boosts;							/* Prevent warning "unused variable" */
 	fgets(line_buffer, MAX_LINE_LENGTH, stdin); /* Read gas level at Start */
@@ -305,8 +302,9 @@ int main()
 	for (row = 0; row < height; ++row) { /* Read map data, line per line */
 		fgets(line_buffer, MAX_LINE_LENGTH, stdin);
 		fputs(line_buffer, stderr);
-		grid[row] = malloc(width * sizeof(char));
+		grid[row] = (char*)malloc((width + 1) * sizeof(char));
 		strcpy(grid[row], line_buffer);
+		grid[row][width] = '\0';
 	}
 	fflush(stderr);
 	fprintf(stderr, "\n=== Race start ===\n");
@@ -319,40 +317,45 @@ int main()
 		sscanf(line_buffer, "%d %d %d %d %d %d", &myX, &myY, &secondX, &secondY, &thirdX, &thirdY);
 		fprintf(stderr, "    Positions: Me(%d,%d)  A(%d,%d), B(%d,%d)\n", myX, myY, secondX, secondY, thirdX, thirdY);
 		fflush(stderr);
-
-		/* Calculate optimal acceleration */
-		accel = calculate_optimal_acceleration(myX, myY, speedX, speedY, gasLevel, width, height, grid);
-		accelerationX = accel.x;
-		accelerationY = accel.y;
-		fprintf(stderr, "    Acceleration: (%d,%d)\n", accelerationX, accelerationY);
-		fflush(stderr);
-
-		/* Detection collisions */
-		/* collision = detect_collisions(myX, myY, speedX, speedY, width, height, grid);
-		if (collision == 0) {
-			fprintf(stderr, " Collision !!!\n");
-		}
-		fflush(stderr); */
-
-		/* Avoid obstacles */
-		/* avoid_obstacles(&accelerationX, &accelerationY, speedX, speedY, myX, myY, width, height, grid);
-		fprintf(stderr, "    Acceleration after obstacle avoidance: (%d,%d)\n", accelerationX, accelerationY);
-		fflush(stderr); */
-
-		/* Find shortest path */
-		pathDistance = find_shortest_path(myX, myY, secondX, secondY, width, height, grid);
-		fprintf(stderr, "    Distance to second: %d\n", pathDistance);
-		fflush(stderr);
-
-		/* Apply boost */
-		apply_boost(&boosts, &accelerationX, &accelerationY, speedX, speedY, myX, myY, width, height, grid);
-
 		/* Gas consumption cannot be accurate here. */
 		gasLevel += gasConsumption(accelerationX, accelerationY, speedX, speedY, 0);
 		speedX += accelerationX;
 		speedY += accelerationY;
-		/* Write the acceleration request to the race manager (stdout). */
 
+		/* Calcul de la position d'arrivée */
+		start.x = myX;
+		start.y = myY;
+		for (x = 0; x < width; x++) {
+			for (y = 0; y < height; y++) {
+				if (grid[y][x] == '=') {
+					end.x = x;
+					end.y = y;
+				}
+			}
+		}
+
+		next = dijkstra_next_move(grid, width, height, start, end);
+		/* accelerationX = next.x - myX;
+		accelerationY = next.y - myY; */
+		initLine(myX, myY, next.x, next.y, &lineInfo);
+		nextPoint(&lineInfo, &currentPoint, 1);
+		accelerationX = currentPoint.x - myX;
+		accelerationY = currentPoint.y - myY;
+
+		if ((currentPoint.x == myX || currentPoint.x == myX - 1 || currentPoint.x == myX + 1) && currentPoint.y == myY) {
+			accelerationX = 0;
+		}
+		if ((currentPoint.y == myY || currentPoint.y == myY - 1 || currentPoint.y == myY + 1) && currentPoint.x == myX) {
+			accelerationX = 0;
+		}
+		/* if (accelerationX == accelerationX_old && accelerationY == accelerationY_old) {
+			accelerationX = 0;
+			accelerationY = 0;
+		}
+		accelerationX_old = accelerationX;
+		accelerationY_old = accelerationY; */
+
+		/* Write the acceleration request to the race manager (stdout). */
 		sprintf(action, "%d %d", accelerationX, accelerationY);
 		fprintf(stdout, "%s", action);
 		fflush(stdout); /* CAUTION : This is necessary  */
@@ -365,6 +368,11 @@ int main()
 			*p = 0;
 		}
 	}
+
+	for (j = 0; j < height; ++j) {
+		free(grid[j]);
+	}
+	free(grid);
 
 	return EXIT_SUCCESS;
 }
