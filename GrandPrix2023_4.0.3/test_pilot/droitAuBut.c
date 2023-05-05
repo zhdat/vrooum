cd944e6dc5efe8ef27cd04026470df35b0c85d52
ravix <calliste.ravix@ecole.ensicaen.fr>
2023-05-05 13:46:23 +0200

    A* vitesse

--- GrandPrix2023_4.0.3/test_pilot/droitAuBut.c
+++ GrandPrix2023_4.0.3/test_pilot/droitAuBut.c
@@ -252,14 +252,16 @@ List* aStar(Node* start, Node* end, char** map, int width, int height, int secon
 							neighbour->g_cost = currentNode->g_cost + 1;
 
 							/* Ajoutez une pénalité pour les virages rapides */
-							double angle1 = calculateAngle(currentNode->x - currentNode->parent->x, currentNode->y - currentNode->parent->y);
-							double angle2 = calculateAngle(newX - currentNode->x, newY - currentNode->y);
-							double angleDifference = fabs(angle2 - angle1);
+							if (currentNode->parent != NULL) { /* Vérifiez si le parent existe */
+								double angle1 = calculateAngle(currentNode->x - currentNode->parent->x, currentNode->y - currentNode->parent->y);
+								double angle2 = calculateAngle(newX - currentNode->x, newY - currentNode->y);
+								double angleDifference = fabs(angle2 - angle1);
 
-							/* Si la différence d'angle est supérieure à un seuil (par exemple, PI/4 radians), ajoutez une pénalité */
-							if (angleDifference > 3.14 / 4) {
-								neighbour->g_cost += 2 * angleDifference; /* Vous pouvez ajuster le coefficient pour modifier l'importance de la
-																			 pénalité */
+								/* Si la différence d'angle est supérieure à un seuil (par exemple, PI/4 radians), ajoutez une pénalité */
+								if (angleDifference > 3.14 / 4) {
+									neighbour->g_cost += 2 * angleDifference; /* Vous pouvez ajuster le coefficient pour modifier l'importance de la
+																			  pénalité */
+								}
 							}
 							if (map[newY][newX] == '~') {
 								neighbour->g_cost = currentNode->g_cost + 4;
