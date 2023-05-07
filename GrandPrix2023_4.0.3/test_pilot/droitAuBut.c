d011894c47e91a389356483f455de95c48dcbfb3
ravix <calliste.ravix@ecole.ensicaen.fr>
2023-05-07 20:35:01 +0200

    Astar

--- GrandPrix2023_4.0.3/test_pilot/droitAuBut.c
+++ GrandPrix2023_4.0.3/test_pilot/droitAuBut.c
@@ -618,8 +618,7 @@ List* aStar(Node* start, Node* end, char** map, int width, int height, int secon
 				}
 
 				neighbour = createNode(newX, newY, currentNode, newSpeedX, newSpeedY, newGas);
-				neighbour->g_cost =
-					currentNode->g_cost + sqrt((newX - currentNode->x) * (newX - currentNode->x) + (newY - currentNode->y) * (newY - currentNode->y));
+				neighbour->g_cost = currentNode->g_cost + 1;
 
 				if (map[newY][newX] == '~') {
 					neighbour->g_cost = currentNode->g_cost + 4;
