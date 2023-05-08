a02b4cb6dad64bf13fc6cc250bd3ee281007cb8b
ravix <calliste.ravix@ecole.ensicaen.fr>
2023-05-08 16:34:50 +0200

    Astar

--- GrandPrix2023_4.0.3/test_pilot/droitAuBut.c
+++ GrandPrix2023_4.0.3/test_pilot/droitAuBut.c
@@ -793,6 +793,8 @@ List* aStar(Node* start, Node* end, char** map, int width, int height, int secon
 		/* Générer les voisins */
 		for (accX = -1; accX <= 1; accX++) {
 			for (accY = -1; accY <= 1; accY++) {
+				currentNode->speedX = currentSpeedX;
+				currentNode->speedY = currentSpeedY;
 				newSpeedX = currentNode->speedX + accX;
 				newSpeedY = currentNode->speedY + accY;
 
