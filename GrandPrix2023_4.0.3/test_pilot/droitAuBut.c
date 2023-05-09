91f296f831e60d31a0904a2876568dc79185e1aa
ravix <calliste.ravix@ecole.ensicaen.fr>
2023-05-09 15:03:51 +0200

    Astar

--- GrandPrix2023_4.0.3/test_pilot/droitAuBut.c
+++ GrandPrix2023_4.0.3/test_pilot/droitAuBut.c
@@ -491,7 +491,7 @@ double heuristicCost(Node* a, Node* b, int accX, int accY)
     /* euclidienne */
     double euclidienne = sqrt(pow(b->x - a->x, 2) + pow(b->y - a->y, 2));
 
-    return fmax(heuristic1, euclidienne);
+    return fmin(heuristic1, euclidienne);
 
 }
 
