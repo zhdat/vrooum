0360632d3943e92fecb707ac8d0173848a73b258
ravix <calliste.ravix@ecole.ensicaen.fr>
2023-05-09 09:22:00 +0200

    Astar

--- GrandPrix2023_4.0.3/test_pilot/droitAuBut.c
+++ GrandPrix2023_4.0.3/test_pilot/droitAuBut.c
@@ -817,7 +817,7 @@ List* aStar(Node* start, Node* end, char** map, int width, int height, int secon
 					continue;
 				}
 
-				if (map[newY][newX] == '~' && SpeedNorme(newSpeedX, newSpeedY) > 1) {
+				if (map[newY][newX] == '~' && SpeedNorme(currentSpeedX, currentSpeedY) > 1) {
 					continue;
 				}
 
