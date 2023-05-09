9fdccdc65d13b4ddb3e2fdca5123d7ba9c3155fc
ravix <calliste.ravix@ecole.ensicaen.fr>
2023-05-09 19:44:23 +0200

    Astar

--- GrandPrix2023_4.0.3/test_pilot/droitAuBut.c
+++ GrandPrix2023_4.0.3/test_pilot/droitAuBut.c
@@ -728,10 +728,9 @@ int shouldExploreNeighbor(Node* currentNode, char** map, int width, int height, 
 		return 0;
 	}
 
-	/* Vérifier qu'il y'a assez d'essence pour finir la course */
-	gasCost = abs(accX) + abs(accY);
-	newGas = currentNode->gas - gasCost;
-	if (newGas < 0) {
+	gasCost = gasConsumption(accX, accY, currentNode->speedX, currentNode->speedY, 0);
+	newGas = currentNode->gas + gasCost;
+	if (newGas < 0 || newGas > maxGas) {
 		return 0;
 	}
 
