c00dbafb0febe76c9754aaa832612dcf071676fc
ravix <calliste.ravix@ecole.ensicaen.fr>
2023-05-09 20:08:04 +0200

    Astar

--- GrandPrix2023_4.0.3/test_pilot/droitAuBut.c
+++ GrandPrix2023_4.0.3/test_pilot/droitAuBut.c
@@ -529,11 +529,18 @@ int isPathClear(char** map, int width, int height, Pos2Dint start, Pos2Dint end)
 {
 	InfoLine line;
 	Pos2Dint point;
+	int direction;
+
+	if (start.x < end.x) {
+		direction = -1;
+	} else {
+		direction = +1;
+	}
 
 	initLine(start.x, start.y, end.x, end.y, &line);
 
 	/* Parcourir les points de la ligne */
-	while (nextPoint(&line, &point, +1) > 0) {
+	while (nextPoint(&line, &point, direction) > 0) {
 		if (point.x < 0 || point.x >= width || point.y < 0 || point.y >= height) {
 			/* Point en dehors des limites de la carte */
 			return 0;
