afce0fd19d6e75a6421ac5097f60da792d82f8c0
ravix <calliste.ravix@ecole.ensicaen.fr>
2023-05-10 11:14:14 +0200

    Astar

--- GrandPrix2023_4.0.3/test_pilot/droitAuBut.c
+++ GrandPrix2023_4.0.3/test_pilot/droitAuBut.c
@@ -592,12 +592,12 @@ ArrayEnd* findEndPositions(char** map, int width, int height, Node* start, Node*
 
 	int endPositionCount = 0;
 	endPositions = (EndPosition*)malloc(sizeof(EndPosition) * width * height);
-	arrayEnd = (ArrayEnd*)malloc(sizeof(ArrayEnd));
+	arrayEnd = (ArrayEnd*)malloc(sizeof(EndPosition));
 
 	for (y = 0; y < height; y++) {
 		for (x = 0; x < width; x++) {
 			if (map[y][x] == '=') {
-				arrayEnd = (ArrayEnd*)realloc(arrayEnd, sizeof(ArrayEnd) * (endPositionCount + 2));
+				arrayEnd = (ArrayEnd*)realloc(arrayEnd, sizeof(EndPosition) * (endPositionCount + 2));
 				Node node;
 				node.x = x;
 				node.y = y;
