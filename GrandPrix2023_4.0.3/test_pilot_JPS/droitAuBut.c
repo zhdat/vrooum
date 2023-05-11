7afdcb5e1e1bc84166347361a1cb30e77bef303c
ravix <calliste.ravix@ecole.ensicaen.fr>
2023-05-11 21:58:20 +0200

    Astar

--- GrandPrix2023_4.0.3/test_pilot_JPS/droitAuBut.c
+++ GrandPrix2023_4.0.3/test_pilot_JPS/droitAuBut.c
@@ -451,7 +451,7 @@ double heuristicCost(Node *a, Node *b, int speedX, int speedY, char **map) {
     double distance = sqrt(pow(a->x - b->x, 2) + pow(a->y - b->y, 2));
     int inSand = (map[a->y][a->x] == '~') ? 1 : 0;
     double expectedGasConsumption = -(gasConsumption(a->x - b->x, a->y - b->y, speedX, speedY, inSand));
-    return distance + (expectedGasConsumption * 10);
+    return distance + expectedGasConsumption;
 }
 
 
