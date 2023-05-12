095802dda53c5494d7378281aa8214b9b232d888
ravix <calliste.ravix@ecole.ensicaen.fr>
2023-05-12 22:37:58 +0200

    Astar

--- GrandPrix2023_4.0.3/test_pilot_JPS/droitAuBut.c
+++ GrandPrix2023_4.0.3/test_pilot_JPS/droitAuBut.c
@@ -718,7 +718,7 @@ Node *createNeighbourNode(int newX, int newY, Node *currentNode, int newSpeedX, 
     if (map[newY][newX] == '~') {
         neighbour->g_cost += 20;
     }
-    if (currentNode->speedX != newSpeedX || currentNode->speedY != newSpeedY) {
+    if (abs(currentNode->speedX) > abs(newSpeedX) || abs(currentNode->speedY) > abs(newSpeedY)) {
         neighbour->g_cost += 10;
     }
 
