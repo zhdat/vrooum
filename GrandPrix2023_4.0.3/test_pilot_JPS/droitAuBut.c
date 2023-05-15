d1fbafef983b52eb007ab53a6b50202a7a2d2df8
ravix <calliste.ravix@ecole.ensicaen.fr>
2023-05-15 15:37:14 +0200

    collision

--- GrandPrix2023_4.0.3/test_pilot_JPS/droitAuBut.c
+++ GrandPrix2023_4.0.3/test_pilot_JPS/droitAuBut.c
@@ -856,6 +856,7 @@ List* aStar(Node* start, const Node* end, char** map, int width, int height, int
 						continue;
 
 					int gasNeeded = gasConsumption(accX, accY, newSpeedX, newSpeedY, map[newY][newX] == '~');
+					fprintf(stderr, "gasNeeded: %d\n", gasNeeded);
 					newGas = currentNode->gas + gasNeeded;
 					if (currentNode->gas + gasNeeded <= 0) {
 						continue;
