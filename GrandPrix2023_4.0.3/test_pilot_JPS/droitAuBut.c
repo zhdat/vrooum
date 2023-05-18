16477893fb4f832d279986cf743ed380e92e22ff
ravix <calliste.ravix@ecole.ensicaen.fr>
2023-05-18 12:07:51 +0200

    collision

--- GrandPrix2023_4.0.3/test_pilot_JPS/droitAuBut.c
+++ GrandPrix2023_4.0.3/test_pilot_JPS/droitAuBut.c
@@ -768,9 +768,6 @@ Node* createNeighbourNode(int newX, int newY, Node* currentNode, int newSpeedX, 
 	if (map[newY][newX] == '~') {
 		factor += 2;
 	}
-	/* if (abs(currentNode->speedX) > abs(newSpeedX) || abs(currentNode->speedY) > abs(newSpeedY)) {
-		factor += 4;
-	} */
 	if (SpeedNorme(newSpeedX, newSpeedY) < 25) {
 		factor += 1;
 	}
@@ -884,7 +881,7 @@ List* aStar(Node* start, const Node* end, char** map, int width, int height, int
 
 					if (!hsContains(closedSet, neighbour)) {
 						Node const* existingNodeInOpenSet = pqFind(openSet, neighbour);
-						if (existingNodeInOpenSet == NULL || neighbour->f_cost < existingNodeInOpenSet->f_cost) {
+						if (existingNodeInOpenSet == NULL || neighbour->g_cost < existingNodeInOpenSet->g_cost) {
 							if (existingNodeInOpenSet != NULL) {
 								pqRemove(openSet, existingNodeInOpenSet);
 							}
