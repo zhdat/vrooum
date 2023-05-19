51c8c40ecd251bef7c4f8a7f1f2450482663c457
ravix <calliste.ravix@ecole.ensicaen.fr>
2023-05-19 21:53:55 +0200

    collision

--- GrandPrix2023_4.0.3/test_pilot_JPS/droitAuBut.c
+++ GrandPrix2023_4.0.3/test_pilot_JPS/droitAuBut.c
@@ -41,7 +41,7 @@ int hsContains(const HashSet* hs, const Node* node)
 	HashSetElement* current = hs->buckets[hash];
 
 	while (current != NULL) {
-		if (nodeEqualsWithoutSpeed(current->node, node)) {
+		if (nodeEquals(current->node, node)) {
 			return 1;
 		}
 		current = current->next;
@@ -147,7 +147,7 @@ void pqRemove(PriorityQueue* pq, const Node* node)
 	if (pq->head == NULL) {
 		return;
 	}
-	if (nodeEqualsWithoutSpeed(pq->head->node, node)) {
+	if (nodeEquals(pq->head->node, node)) {
 		PriorityQueueElement* elementToRemove = pq->head;
 		pq->head = pq->head->next;
 		free(elementToRemove);
@@ -155,7 +155,7 @@ void pqRemove(PriorityQueue* pq, const Node* node)
 	}
 	current = pq->head;
 	while (current->next != NULL) {
-		if (nodeEqualsWithoutSpeed(current->next->node, node)) {
+		if (nodeEquals(current->next->node, node)) {
 			PriorityQueueElement* elementToRemove = current->next;
 			current->next = current->next->next;
 			free(elementToRemove);
